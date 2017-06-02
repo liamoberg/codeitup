52aaa9a38d6fac6d1b91f7d3086c536cbc235eb4
Liam Öberg <obergliam@gmail.com>
2017-06-02 14:56:49 +0200

    added nav and info section

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ $(document).ready(function() {
             opacity: 1
     }, 1500);
 });
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+
+    if($(window).scrollTop()>100)
+    $(".nav-menu").css({"background-color" : "rgba(0,0,0,.5)"});
+
+    else
+    $(".nav-menu").css({"background-color" : "rgba(0,0,0,1)"});
+
+
+    });
+
+});
