edf562e52a654334ae7352c40df0395eee38225d
Liam Öberg <obergliam@gmail.com>
2017-06-05 17:27:19 +0200

    added logos, changed nav and some jquery functions

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 $(document).ready(function() {
     $('.startlogo').animate({
-            left: '-1200px',
+            left: '-600px',
             opacity: 1
     }, 1500);
-});
-
-$(document).ready(function(){
 
     $(window).scroll(function(){
+        if($(window).scrollTop()>100)
+            $(".nav-menu").css({"background-color" : "rgba(0,0,0,.5)"});
+        else 
+            $(".nav-menu").css({"background-color" : "rgba(0,0,0,0)"});
+            
+    });
+   
+    $(window).scroll(function(){
+        if($(window).scrollTop()>100)
+            $(".menu-item").css({"color" : "#FFFFFF"});
+        else 
+            $(".menu-item").css({"color" : "#000000"});
+            
+    });
 
-    if($(window).scrollTop()>100)
-    $(".nav-menu").css({"background-color" : "rgba(0,0,0,.5)"});
-
-    else
-    $(".nav-menu").css({"background-color" : "rgba(0,0,0,1)"});
-
-
+    $(window).scroll(function(){
+        if($(window).scrollTop()>500)
+            $(".info-logo").fadeIn("slow");
+        else
+            $(".info-logo").fadeOut("slow");
     });
 
 });
